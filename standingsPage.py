cbd5d9c2343789c7286da11100b827f82b36730e
Michael Lucas <lucas.michael02125@gmail.com>
2025-11-18 17:10:42 +0000

    20251118 - Finishing up rest of standing Page

--- standingsPage.py
+++ standingsPage.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from Pages.standings import Ui_Standings
 from datetime import datetime
 import teams as teamAPI
+import ctypes
 
 import statsapi
 
@@ -22,20 +23,56 @@ class StandingsPage(QWidget):
         self.ui.ckBx_WildCard.stateChanged.connect(lambda:self.adjust_standingCols())
         self.ui.cWdg_pickDate.clicked.connect(lambda:self.getStandings_ByDate(self.ui.cWdg_pickDate.selectedDate()))
         self.ui.cmbBx_StandingFilter.currentTextChanged.connect(lambda: self.showFilter(self.ui.cmbBx_StandingFilter.currentText()))
+        self.ui.cmBx_yearFilter.currentTextChanged.connect(lambda: self.getStandings_ByYear(self.ui.cmBx_yearFilter.currentText()))
 
     def getStandings_ByDate(self,chosenDate):
-        chosenDate = chosenDate.toString('MM/dd/yyyy')
+        print(chosenDate)
+        theChosenDate = chosenDate.toString('MM/dd/yyyy')
         isWC = self.ui.ckBx_WildCard.isChecked()
-        leagueData = teamAPI.get_LeagueData(Date=chosenDate, InclWC=isWC)
+        leagueData = teamAPI.get_LeagueData(Date=theChosenDate, InclWC=isWC)
+        if not leagueData:
+            ctypes.windll.user32.MessageBoxW(0, "No data for chosen date", "Please try again", 1)
+            return
         teamAPI.load_ALEast(self,leagueData=leagueData)
-        teamAPI.load_ALCentral(self,leagueData=leagueData)
         teamAPI.load_ALWest(self,leagueData=leagueData)
         teamAPI.load_NLEast(self,leagueData=leagueData)
-        teamAPI.load_NLCentral(self,leagueData=leagueData)
         teamAPI.load_NLWest(self,leagueData=leagueData)
+        if int(chosenDate.year()) > 1994:
+            self.ui.tblWid_ALCentral.show()
+            self.ui.tblWid_NLCentral.show()
+            self.ui.lbl_ALCentral.show()
+            self.ui.lbl_NLCentral.show()
+            teamAPI.load_NLCentral(self,leagueData=leagueData)
+            teamAPI.load_ALCentral(self,leagueData=leagueData)
+        else:
+            self.ui.tblWid_ALCentral.hide()
+            self.ui.tblWid_NLCentral.hide()
+            self.ui.lbl_ALCentral.hide()
+            self.ui.lbl_NLCentral.hide()
 
 
-        
+    def getStandings_ByYear(self, chosenYear):
+        isWC = self.ui.ckBx_WildCard.isChecked()
+        leagueData = teamAPI.get_LeagueData( Season=int(chosenYear))
+        teamAPI.load_ALEast(self,leagueData=leagueData)
+        teamAPI.load_ALWest(self,leagueData=leagueData)
+        teamAPI.load_NLEast(self,leagueData=leagueData)
+        teamAPI.load_NLWest(self,leagueData=leagueData)
+        if int(chosenYear) > 1994:
+            self.ui.tblWid_ALCentral.show()
+            self.ui.tblWid_NLCentral.show()
+            self.ui.lbl_ALCentral.show()
+            self.ui.lbl_NLCentral.show()
+            teamAPI.load_NLCentral(self,leagueData=leagueData)        
+            teamAPI.load_ALCentral(self,leagueData=leagueData)
+        else:
+            self.ui.tblWid_ALCentral.hide()
+            self.ui.tblWid_NLCentral.hide()
+            self.ui.lbl_ALCentral.hide()
+            self.ui.lbl_NLCentral.hide()            
+            
+
+
     def showFilter(self,value):
         if value == "Season":
             self.ui.cWdg_pickDate.setVisible(False)
@@ -49,7 +86,6 @@ class StandingsPage(QWidget):
 
 
     def adjust_standingCols(self):
-        print(bool(self.ui.ckBx_WildCard.isChecked()))
         if bool(self.ui.ckBx_WildCard.isChecked()) == True:
             for col in [6,7,5]:
                 self.ui.tblWid_ALEast.setColumnHidden(col,False)
@@ -58,7 +94,7 @@ class StandingsPage(QWidget):
                 self.ui.tblWid_NLEast.setColumnHidden(col,False)
                 self.ui.tblWid_NLCentral.setColumnHidden(col,False)
                 self.ui.tblWid_NLWest.setColumnHidden(col,False)               
-
+        else:
             for col in [6,7,5]:
                 self.ui.tblWid_ALEast.setColumnHidden(col,True)
                 self.ui.tblWid_ALCentral.setColumnHidden(col,True)
